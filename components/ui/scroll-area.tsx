3c726d70e7b2bf4438ac78a7ee169f8bb2da86b4
User <user@example.com>
2026-02-24 21:30:18 -0500

    \fix: syntax errors in Tabs and ScrollArea components"

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -37,10 +37,10 @@ const ScrollBar = React.forwardRef<
             "h-2.5 flex-col border-t border-t-transparent p-[1px]",
             className
         )}
-    ...props
+        {...props}
     >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
-  </ScrollAreaPrimitive.ScrollAreaScrollbar >
+        <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    </ScrollAreaPrimitive.ScrollAreaScrollbar >
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
 
